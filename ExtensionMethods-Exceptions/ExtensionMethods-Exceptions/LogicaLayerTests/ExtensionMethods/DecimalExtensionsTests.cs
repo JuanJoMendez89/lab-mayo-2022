c0dc707c5c0d74288d69f1fbde86e34a42b95298
JuanJoMendez89 <106120530+JuanJoMendez89@users.noreply.github.com>
2022-05-31 23:19:10 -0300

    Argegados un par de unit tests

---
+++ ExtensionMethods-Exceptions/ExtensionMethods-Exceptions/LogicaLayerTests/ExtensionMethods/DecimalExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicaLayer.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaLayer.ExtensionMethods.Tests
+{
+    [TestClass()]
+    public class DecimalExtensionsTests
+    {
+        [TestMethod()]
+        public void DividirTest()
+        {
+            decimal valor = 8;
+            decimal valor2 = 2;
+            decimal resultadoEsperado = 4;
+
+            decimal resultadoActual = valor.Dividir(valor2);
+
+            Assert.AreEqual(resultadoEsperado, resultadoActual);
+        }
+    }
+}
---
+++ ExtensionMethods-Exceptions/ExtensionMethods-Exceptions/LogicaLayerTests/LogicTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicaLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaLayer.Exceptions;
+
+namespace LogicaLayer.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void DispararExcepcionPersonalizadaTest()
+        {
+            Action action = Logic.DispararExcepcionPersonalizada;
+
+            Assert.ThrowsException<ExcesoDeFachaException>(action);
+        }
+    }
+}
